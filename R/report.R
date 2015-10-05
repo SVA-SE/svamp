66a5b916b2f735b9f13331fbcae5fc63db37ad6f
Giampaolo Cocca <giampaolo.cocca@sva.se>
2015-10-05 13:36:17 +0200

    built the function draw_map

--- R/report.R
+++ R/report.R
@@ -1,7 +1,71 @@
+#' Draw a map
+#'
+#' Draw a map of outbreak points over the Län of Sweden.
+#' @param outbreak Scalar or numeric vector with the PPNs number to draw
+#' @return A .jpeg? map
+#' @keywords internal
+draw_map <- function(ppn, 
+                     ppn_obj = system.file("extdata/result.rda",
+                                           package = "svamp")) {
+#   if(exists(ppn)){
+#     stop("No ppn number provided")
+#   } 
+  
+  data(NUTS_03M, package = "svamp", envir = .svamp_env)
+  NUTS_03M <- report_data_object()$NUTS_03M
+   
+  # load results from svdc package
+  load(ppn_obj)
+  farms_RT90 <- result$farms_RT90
+  nuts_label <- result$nuts_label
+
+  outbreak <- subset(farms_RT90, farms_RT90$Ppn == ppn)
+  NUTS_03M <- NUTS_03M[NUTS_03M$STAT_LEVL_ == 3, 1:2]
+   
+  # drop levels without data in NUTS_ID 
+  NUTS_03M@data$NUTS_ID <- droplevels(NUTS_03M@data$NUTS_ID) 
+  
+  NUTS_03M@data$Lan <- nuts_label$Swedish[match(as.character(NUTS_03M@data$NUTS_ID),
+                                                nuts_label$NUTS)]
+  
+  # Check for points falling outside Swedish border
+  if(all(!is.na(over(outbreak, NUTS_03M)))) {
+    
+    lan <- over(outbreak, NUTS_03M)
+    lan <- NUTS_03M[NUTS_03M@data$NUTS_ID %in% lan$NUTS_ID, ]
+    plot(NUTS_03M, axes = T)
+    plot(lan, add = T, col = "yellow")
+    
+    
+    points(outbreak, pch = 19, col = "red", cex = 1)
+    
+    # Add labels to the polygons
+    
+    invisible(text(coordinates(NUTS_03M),
+                   labels = as.character(NUTS_03M@data$Lan), 
+                   cex = 0.8))
+    
+    title(expression(paste("Geographic location of the PPN under investigation")))
+    
+    legend("bottomright",
+           cex = 1.3, 
+           "PPN location", 
+           pch = 19, 
+           pt.cex = 1.2,
+           col = "red",
+           bty = "n")
+    
+  } else {
+    
+    plot(1,1,col = "white")
+    text(1,1,"Some coordinates fall outside the Swedish borders")
+  }
+}
+  
 #' Produce an .html report
 #'
 #' Produce an .html report with information for outbreak/suspicion management
-#' @param ppn ppn number (single scalar value)
+#' @param ppn Ppn number (single scalar value)
 #' @param ppn_obj Path to the list of objects output from svdc package
 #' @param firstname Firstname of the person running the report
 #' @param lastname Lastname of the person running the report
@@ -115,3 +179,5 @@ report <- function(ppn = 94403,
 report_data_object <- function() {
   .svamp_env
 }
+
+
