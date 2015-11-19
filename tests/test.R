25611077798806046ac3f34074e045ae1d6802cf
Thomas Rosendal <thomas.rosendal@sva.se>
2015-11-19 11:48:57 +0100

    Added a test that fails

---
+++ tests/test.R
@@ -0,0 +1,14 @@
+library(svamp)
+
+## test_1
+
+report(153265)
+
+## test_2
+
+## res <- tools::assertError(
+##   foo(parameter that causes error)
+## )
+
+## stopifnot(length(grep("The expected error message",
+##                      res[[1]]$message)) > 0)
